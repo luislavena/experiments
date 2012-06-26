f5a84ac4ce27e2d1b8bf768b060aea2aeb6940ca
Luis Lavena <luislavena@gmail.com>
2012-06-25 21:45:10 -0300

    Use LD function wrapping instead
    Instead of rely on linker ordering and disable an important feature like
    function duplication, we will rely on LD ability to wrap a function.

--- patch-static-lib/patch.c
+++ patch-static-lib/patch.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
-extern void awesome(void);
+/* we need to define __real_awesome to use it */
+void __real_awesome();
 
-void awesome(void)
+void __wrap_awesome(void)
 {
 	puts("Awesome from replacement!");
+
+	/* here you can call the original awesome() function */
+	/* __real_awesome(); */
 }
